77e1e819fe23fa4fea9976de8f44e99493faabba
adityakiran1423 <adi.kiran@gmail.com>
2024-09-18 14:49:03 +0530

    send correlation id

--- app/server.go
+++ app/server.go
@@ -20,9 +20,16 @@ func main() {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
+	
+	var buff = make([]byte, 8)
+
+	conn.Read(buff)
+
+	conn.Write([]byte{0,0,0,0,0,0,0,7});
 }
