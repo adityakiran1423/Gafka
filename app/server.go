f50f8d770bda6f961fa38b147228b19441e133fa
adityakiran1423 <adi.kiran@gmail.com>
2024-09-23 12:28:00 +0530

    used bin/encoding

--- app/server.go
+++ app/server.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
-	// "encoding/binary" // added externally
+	"encoding/binary" // added externally
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
@@ -34,33 +34,57 @@ func main() {
 	fmt.Printf("message_length is %x\n", message_length)
 	correlation_id := append(message_length, buff[8:12]...)
 
-	byte_api_version := make([]byte, 2)
-	copy(byte_api_version, buff[6:8])
+
+
+	request_api_version := binary.BigEndian.Uint16(buff[6:8])
+
+
+	if request_api_version > 0 && request_api_version <= 4{
+		kafka_response := append(correlation_id, buff[6:8]...)
+		conn.Write(kafka_response)
+	}
+	kafka_response :=append(correlation_id, []byte{'1','0','0','0','1','1'}...)
+	conn.Write(kafka_response)
+	// byte_api_version := make([]byte, 2)
+	// copy(byte_api_version, buff[6:8])
 	// fmt.Println(correlation_id)
-	api_version := parseApiVersion(byte_api_version)
-	fmt.Printf("api_version recieved from func is %s\n", api_version)
-	fmt.Printf("api_version recieved from func in hex is %x\n", api_version)
+	// fmt.Printf("hex val of api from request %x\n", byte_api_version)
+	// api_version := parseApiVersion(request_api_version)
+	// fmt.Printf("api_version recieved from func is %s\n", api_version)
+	// fmt.Printf("api_version recieved from func in hex is %x\n", api_version)
 	// int_api_verion := binary.BigEndian.Uint64(byte_api_version)
 
-	kafka_response := append(correlation_id, api_version...)
+	
 	// fmt.Printf("eventually this is sent (string format) %s \n", kafka_response)
 	// fmt.Printf("eventually this is sent (byte format) %b \n", kafka_response)
-	fmt.Printf("eventually this is sent (hex format) %x \n", kafka_response)
-	conn.Write(kafka_response)
+	// fmt.Printf("eventually this is sent (hex format) %x \n", kafka_response)
+	// conn.Write(kafka_response)
 	// conn.Write(api_version)
 }
 
-func parseApiVersion(message []byte) []byte {
-	// func parseApiVersion(message []byte) int{
-	fmt.Printf("api from the request message in hex is %x\n", message)
-	// fmt.Printf("second hex's decimal value is %d\n", message[1])
-	api_version := message[0] + message[1]
-	// fmt.Println(api_version)
-	if api_version > 0 && api_version <= 4 {
-		return message
-	}
-	error_api_return_message := make([]byte, 4)
-	copy(error_api_return_message, []byte{'0','1','0','1','0','0','1','1'})
-	fmt.Printf("The api return if api version isn't 0 to 4 is %x\n", error_api_return_message)
-	return error_api_return_message
-}
+// func parseApiVersion(message []byte) []byte {
+
+// 	decimal_num, err := strconv.ParseInt(message, 16, 64)
+
+// 	// in case of any error
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	// print the decimal number
+// 	fmt.Println("The decimal conversion of", hexadecimal_num, "is ", decimal_num)
+
+// 	// func parseApiVersion(message []byte) int{
+// 	fmt.Printf("api from the request message in hex is %x\n", message)
+// 	// fmt.Printf("second hex's decimal value is %d\n", message[1])
+// 	api_version := message[0] + message[1]
+// 	fmt.Printf("api_version which I calculated %x\n", api_version)
+// 	// fmt.Println(api_version)
+// 	if api_version > 0 && api_version <= 4 {
+// 		return message
+// 	}
+// 	error_api_return_message := make([]byte, 4)
+// 	copy(error_api_return_message, []byte{'0','1','0','1','0','0','1','1'})
+// 	fmt.Printf("The api return if api version isn't 0 to 4 is %x\n", error_api_return_message)
+// 	return error_api_return_message
+// }
