90005cbea7d63967c7fa0f8e5b6f74210ee7c726
adityakiran1423 <adi.kiran@gmail.com>
2024-09-23 10:27:21 +0530

    parse api version changed return from 0003 to bin(35)

--- app/server.go
+++ app/server.go
@@ -31,20 +31,31 @@ func main() {
 
 	message_length := make([]byte, 4)
 	copy(message_length, []byte{'0', '0', '0', '0'})
+	fmt.Printf("the message_length byte array is %b\n", message_length)
 	correlation_id := append(message_length, buff[8:12]...)
 
 	byte_api_version := make([]byte, 2)
 	copy(byte_api_version, buff[6:8])
-
-	parseApiVersion(byte_api_version)
-
+	fmt.Println(correlation_id)
+	api_version := parseApiVersion(byte_api_version)
+	fmt.Printf("api_version recieved from func is %s", api_version)
 	// int_api_verion := binary.BigEndian.Uint64(byte_api_version)
 
 	conn.Write(correlation_id)
+	conn.Write(api_version)
 }
 
-
-func parseApiVersion(message []byte) []byte{
-	fmt.Println(message)
-	return message
+func parseApiVersion(message []byte) []byte {
+	// func parseApiVersion(message []byte) int{
+	fmt.Printf("first hex's decimal value is %d\n", message[0])
+	fmt.Printf("second hex's decimal value is %d\n", message[1])
+	api_version := message[0] + message[1]
+	fmt.Println(api_version)
+	if api_version > 0 && api_version <= 4 {
+		return message
+	}
+	error_api_return_message := make([]byte, 4)
+	copy(error_api_return_message, []byte{'0','1','0','1','0','0','1','1'})
+	fmt.Printf("The api return if api version isn't 0 to 4 is %d", error_api_return_message)
+	return error_api_return_message
 }
